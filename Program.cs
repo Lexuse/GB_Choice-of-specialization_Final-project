25c3e57de3883e32eae9b32ce6f58b2ffc7c7ece
Lexuse <Lexuse2007@gmail.com>
2023-01-02 12:41:23 +0300

    add initial

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿//программа  из имеющегося массива строк формирует новый массив из строк, длина которых  равна 3 символам.
+Console.Clear();
+
+string[] array = GenerateArray();
+string[] array2 = new string[array.Length];
+CutArrayTo3(array, array2);
+PrintArray(array2);
+
+
+
